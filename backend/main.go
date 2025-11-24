2f492cfaed21c789d1d8947f4c2d623c7c9986c1
Ben <benjaminlloyd2006@gmail.com>
2025-11-24 19:51:18 +0000

    Feat: adding initial Todo struct and in memory storage

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,19 @@
 package main
 
 import "net/http"
+import "encoding/json"
+
+/* 	initial Todo struct, representing a single item on a to-do list. 
+	Could be expanded with additional fields, like times/dates, priorities, etc.) if wanted.	*/
+type Todo struct {
+
+	// using JSON "struct tags" so that when the struct is converted to/from JSON the field names are kept.
+    Title       string `json:"title"`
+    Description string `json:"description"`
+}
+
+// basic array to hold all Todos in memory
+var todos []Todo
 
 func main() {
 	// Your code here
